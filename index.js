fe2cdbdd673b3b1935872dd2cd7836d1a99f02cb
Savin Osuka <osukasavin2001@gmail.com>
2022-06-03 09:14:47 +0300

    Mongoose connection to mongoDB

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const  mongoose = require('mongoose')
 const app = express()
 const PORT = 8000
 
+// Mongoose & MongoDB  connection
+const databaseUrl = 'mongodb://localhost:27017/ussd'
+mongoose.connect(databaseUrl)
+const db = mongoose.connection
 
+db.on('error', (err)=> {
+    console.log(err)
+})
+
+db.once('open', ()=> {
+    console.log("Database running")
+})
 
 
 
