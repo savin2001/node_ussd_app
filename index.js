72d0509fece37919f206536c82d61b98066a9fd2
Savin Osuka <osukasavin2001@gmail.com>
2022-06-03 11:11:19 +0300

    Switching back from heroku to github

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const  mongoose = require('mongoose')
-const app = express()
-const PORT = 8000
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = 8000;
 
 // Mongoose & MongoDB  connection
-const databaseUrl = 'mongodb://localhost:27017/ussd'
-mongoose.connect(databaseUrl)
-const db = mongoose.connection
+const databaseUrl = "mongodb://localhost:27017/ussd";
+mongoose.connect(databaseUrl);
+const db = mongoose.connection;
 
-db.on('error', (err)=> {
-    console.log(err)
-})
+db.on("error", (err) => {
+    console.log(err);
+});
 
-db.once('open', ()=> {
-    console.log("Database running")
-})
-
+db.once("open", () => {
+    console.log("Database running");
+});
 
 // Body-parser
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// When the application runs
+app.get("/", (req, res) => res.send("Hello World!"));
+app.post("/", (req, res) => {
+    const { phoneNumber, sessionId, text } = req.body;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.post('/', (req, res) => {
-    console.log(req.body)
+    // Checking if there is a response from the USSD
+    let response;
+
+    if (text === "") {
+        response = "CON Enter your first name";
+    } else if (text !== "") {
+    }
+
+    setTimeout(() => {
+        console.log(text);
+        res.send(response);
+        res.end();
+    }, 2000);
 });
